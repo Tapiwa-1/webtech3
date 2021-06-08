e50975a945bd9fba00be688477d503b2fdf2ef26
Tapiwa Motsi <83808601+Tapiwa-1@users.noreply.github.com>
2021-06-08 15:32:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+        function myFuction()
+        {
+            var change = document.getElementsByClassName("zebra");
+        
+        for (var i = 0; i <change.length;i++)
+        {
+            change[i].style.backgroundColor = "black";
+            change[i].style.color = "white";
+        }
+        
+        }
